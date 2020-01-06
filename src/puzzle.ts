57a25be7649b3781f6cf7e7767e490eb3365199b
tailuge <tailuge@gmail.com>
2020-01-06 16:04:20 +0000

    redraw vnode

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -2,20 +2,25 @@ import { Chess } from "chess.js"
 import { Chessground } from "chessground"
 import { Color } from "chessground/types"
 import { toDests, toColor } from "./util"
+import { h, init } from "snabbdom"
 import { VNode } from "snabbdom/vnode"
-import { h } from "snabbdom"
+import klass from "snabbdom/modules/class"
+import attributes from "snabbdom/modules/attributes"
+import props from "snabbdom/modules/props"
+import listeners from "snabbdom/modules/eventlisteners"
 
 export class Puzzle {
+  patch = init([klass, props, attributes, listeners])
+
   readonly analysis
   private readonly chess
   private readonly config
-  private status
+  vnode: VNode
 
   constructor(analysis) {
     this.analysis = analysis
     this.chess = new Chess(this.analysis.fen)
     this.config = this.initialiseConfig()
-    this.status = ""
   }
 
   initialiseConfig() {
@@ -32,12 +37,8 @@ export class Puzzle {
     }
   }
 
-  pathFromStatus(path) {
-    return this.status == "" ? path : path + "." + this.status
-  }
-
   render() {
-    return h(this.pathFromStatus("section.blue.merida"), [
+    return h("section.blue.merida", [
       h("div.cg-board-wrap", {
         hook: {
           insert: this.runUnit,
@@ -60,11 +61,15 @@ export class Puzzle {
     ])
   }
 
-  url(analysis) {
-    const chess = new Chess(analysis.fen)
-    let color: Color = toColor(chess)
-    return `https://lichess.org/${analysis.id}/${color}#${analysis.halfMove -
-      1}`
+  redraw() {
+    this.vnode = this.patch(this.vnode, this.render())
+  }
+
+  runUnit = (vnode: VNode) => {
+    const el = vnode.elm as HTMLElement
+    el.className = "cg-board-wrap"
+    this.vnode = vnode
+    this.run(el)
   }
 
   run(el) {
@@ -106,21 +111,27 @@ export class Puzzle {
       },
       drawable: {
         shapes: [
-          { orig: orig, dest: dest, brush: "yellow" },
-          this.arrow(this.analysis.move, "red"),
-          this.arrow(this.analysis.best, "green")
+          { orig: orig, dest: dest, brush: "yellow" }
+          //,
+          //this.arrow(this.analysis.move, "red"),
+          //this.arrow(this.analysis.best, "green")
         ]
       }
     })
-  }
 
-  runUnit = (vnode: VNode) => {
-    const el = vnode.elm as HTMLElement
-    el.className = "cg-board-wrap"
-    this.run(el)
+    this.analysis.judgment.name = " ... "
+    console.log("redraw")
+    this.redraw()
   }
 
   arrow(move, colour) {
     return { orig: move.from, dest: move.to, brush: colour }
+  }
+
+  url(analysis) {
+    const chess = new Chess(analysis.fen)
+    let color: Color = toColor(chess)
+    return `https://lichess.org/${analysis.id}/${color}#${analysis.halfMove -
+      1}`
   }
 }
